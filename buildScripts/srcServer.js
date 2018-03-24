d8b6991d6b6b720cff85c301542a48e427598aa1
jonocosa <jonocosa@gmail.com>
2018-03-24 16:03:59 +0000

    added json

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,17 +1,33 @@
-var express = require("express");
-var path = require("path");
-var open = require("open");
+import express from 'express';
+import path from "path";
+import open from "open";
+import webpack from "webpack";
+import config from "../webpack.config.dev";
 
 const port = 3000;
-let app = express();
+const app = express();
+const compiler = webpack(config);
+
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}));
 
 app.get('/', (req, res)=>{
   res.sendFile(path.join(__dirname, '../src/index.html'))
 });
 
+app.get('/users', function(req, res) {
+  res.json([
+    {"id": 1,"firstName": "Bob","lastName": "Smith", "email":"bob@gmail.com"},
+    {"id": 2,"firstName": "Luke","lastName": "Skywalker", "email":"luk@rebel.com"},
+    {"id": 3,"firstName": "Mike","lastName": "Tyson", "email":"IronMike@gmail.com"}
+  ]);
+});
+
 app.listen(port, (err)=>{
   if(err) {
-    console.log(err)
+    console.log(err) // eslint-disable-line no-console
   }else {
     open('http://localhost:' + port)
   }
---
+++ src/index.test.js
@@ -0,0 +1,22 @@
+import {expect} from 'chai';
+import jsdom from 'jsdom';
+import fs from 'fs';
+
+describe('Our first test', ()=> {
+  it("sould pass", ()=> {
+    expect(true).to.equal(true)
+  })
+});
+
+describe("index.html", ()=> {
+  it('sould say hello', (done)=> {
+    const index = fs.readFileSync('./src/index.html', "utf-8");
+
+    jsdom.env(index, function(err, window) {
+      const h1 = window.document.getElementByTagName('h1')[0];
+      expect(h1.innerHTML).to.equal("Hello World!");
+      done();
+      window.close();
+    });
+  })
+})
